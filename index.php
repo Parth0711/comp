a79c67c84e9c85f06742a90b26f1a56b8dc38df1
Parth0711 <patelparthalarsa@gmail.com>
2020-03-26 22:11:21 -0700

    our initial codebase

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" class="home">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+
+    <title>The Registery</title>
+  </head>
+
+  <body class="home">
+    <?php include_once('notification.php') ?>
+    
+    <div class="jumbotron">
+      <header>
+        <h1 class="display-4">The Registration page</h1>
+      
+        <hr class="my-4">
+   
+        <a class="btn" href="register.php">Register</a>
+        <a class="btn" href="login.php">Login</a>
+      </header>
+
+      <section class="mb-5">
+      </section>
+    </div>
+  </body>
+</html>
